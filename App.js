c310ee4bce876bbe7988420d392042e74fea8443
System Administrator <root@Macbook-Pro.local>
2021-03-07 18:46:54 -0600

    :tada: First Commit

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ export default function App() {
 
   const addGoalHandler = () => {
     setCourseGoals([...courseGoals, enteredGoal]);
-    setEnteredGoal('');
+    setEnteredGoal(''):
   }
 
   return (
